197e1c0503e213a2f9dd85886df4754f125800fa
Huann Almeida <huannvictor@gmail.com>
2021-12-08 16:57:13 -0300

    changing modal according to the buttons' behavior

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,18 +2,34 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
-// when the option 'marcar com lido' is clicked
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal .buttons button')
 
+// setting activity buttons
 const checkButtons = document.querySelectorAll('.actions a.check')
-
-checkButtons.forEach(button => {
-  button.addEventListener('click', event => modal.open())
-})
-
-// when the option 'excluir' is clicked
-
 const deleteButtons = document.querySelectorAll('.actions a.delete')
 
-deleteButtons.forEach(button => {
-  button.addEventListener('click', event => modal.open())
+// catching button activities 
+// changing modal according to the buttons' behavior 
+
+checkButtons.forEach(button => {
+  button.addEventListener('click', handleClick)
 })
+
+deleteButtons.forEach(button => {
+  button.addEventListener('click', (event) => handleClick(event, false))
+})
+
+// setting the modal's changing according to the buttons' behavior 
+
+function handleClick (event, check = true){
+  modalTitle.innerHTML = check ? 'Marcar como lida' : 'Excluir pergunta'
+  modalDescription.innerHTML = check 
+    ? 'Tem certeza que você deseja marcar esta pergunta como lida?'
+    : 'Tem certeza que você deseja excluir esta pergunta?'
+  modalButton.innerHTML = check ? 'Sim' : 'Sim, excluir'
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+  modal.open()
+}
