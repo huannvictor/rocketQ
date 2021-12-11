6808eda2c072650cd95b0ccc060c9266b15a1d58
Huann Almeida <huannvictor@gmail.com>
2021-12-10 22:33:53 -0300

    setted route when 'entrar na sala' is clicked

--- src/route.js
+++ src/route.js
@@ -4,11 +4,14 @@ const RoomController = require('./controllers/roomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
-route.get('/create-pass.ejs', (req, res) => res.render('index', {page: 'create-pass'}))
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+route.get('/create-pass.ejs', (req, res) =>
+  res.render('index', { page: 'create-pass' })
+)
 
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
 
 route.post('/question/create/:room', QuestionControler.create)
 route.post('/question/:room/:question/:action', QuestionControler.index)
